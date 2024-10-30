983d28a5d5eb51dc6cc169470a58b33d59fe9473
Rahul Gopinathan <rahulthettikulam@gmail.com>
2024-10-30 10:00:03 +0000

    updated default port Created a new end-point

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
 import express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const server = app.listen(port, () => {
-  console.log("App started successfully");
+  console.log("App started successfully!!!!!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello From Hoi Hoi Express!!!!!!!");
 });
